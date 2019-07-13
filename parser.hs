2a7cf6a9f66b4d0431d797493e238d88192edce0
Andrei Nitu <an16228@my.bristol.ac.uk>
2019-07-13 18:16:09 +0100

    Abandon the Yoda library

--- parser.hs
+++ parser.hs
@@ -1,8 +1,72 @@
 module Parser where
 
-import Yoda
 import Data.List
+import Control.Applicative
 import Prelude hiding (Num)
+
+newtype Parser a = Parser (String -> [(String, a)])
+
+parse :: Parser a -> String -> [(String, a)]
+parse (Parser px) ts = px ts
+
+item :: Parser Char
+item = Parser (\ts -> case ts of
+  []      -> []
+  (t:ts') -> [(ts', t)])
+
+instance Functor Parser where
+--fmap :: (a -> b) -> Parser a -> Parser b
+  fmap f (Parser px) = Parser (\ts -> [(ts', f x) | (ts', x) <- px ts])
+
+instance Applicative Parser where
+--pure :: a -> Parser a
+  pure x = Parser (\ts -> [(ts, x)])
+
+--(<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  Parser pf <*> Parser px = Parser (\ts -> [(ts'', f x) | (ts',  f) <- pf ts,
+                                                          (ts'', x) <- px ts'])
+
+-- (<:>) :: Applicative f => f a -> f [a] -> f [a]
+px <:> pxs = (:) <$> px <*> pxs
+
+instance Alternative Parser where
+--empty :: Parser a
+  empty = Parser (\ts -> [])
+
+--(<|>) :: Parser a -> Parser a -> Parser a
+  Parser px <|> Parser py = Parser (\ts -> px ts ++ py ts)
+
+instance Monad Parser where
+--return :: a -> Parser a
+  return = pure
+
+--(>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  Parser px >>= f = Parser (\ts -> concat [parse (f x) ts' | (ts', x) <- px ts])
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = item >>= \t -> if p t then pure t else empty
+
+char :: Char -> Parser Char
+char c = satisfy (c ==)
+
+oneOf :: [Char] -> Parser Char
+oneOf = satisfy . flip elem
+
+string :: String -> Parser String
+string []     = return ""
+string (c:cs) = char c <:> string cs
+
+whitespace :: Parser ()
+whitespace = many (oneOf [' ', '\t']) *> pure ()
+
+tok :: String -> Parser String
+tok xs = whitespace *> string xs <* whitespace
+
+num :: Parser Integer
+num = read <$> some (oneOf ['0' .. '9']) <* whitespace
+
+var :: Parser String
+var = some (oneOf ['a' .. 'z']) <* whitespace
 
 -- syntactic categories Num and Var
 type Num = Integer
@@ -44,7 +108,7 @@ precedence ops arg = foldl' build arg ops
   where build term ops = chainl term ops
 
 aexp = precedence [Mult <$ tok "*"
-                  ,Add  <$ tok "+" <|> Sub <$ tok "-" ]
+                  ,Add  <$ tok "+" <|> Sub <$ tok "-"]
      ( Num <$> num
    <|> Var <$> var
    <|> tok "(" *> aexp <* tok ")")
@@ -72,15 +136,3 @@ chainl p op = p >>= rest where
               y <- p
               rest (f x y)
        <|> return x
-
-whitespace :: Parser ()
-whitespace = many (oneOf [' ', '\t']) *> pure ()
-
-tok :: String -> Parser String
-tok xs = whitespace *> string xs <* whitespace
-
-num :: Parser Integer
-num = read <$> some (oneOf ['0' .. '9']) <* whitespace
-
-var :: Parser String
-var = some (oneOf ['a' .. 'z']) <* whitespace
--- while.hs
+++ while.hs
@@ -1,25 +1,24 @@
-import Prelude hiding (Num)
 import qualified Prelude (Num)
+import Prelude hiding (Num)
 import Data.List
 import Data.Maybe
 import Parser
-import Yoda
 
 -- Set of all possible states as the set of
 -- all possible functions from variables to integers
-type State = Var->Z
+type State = Var -> Z
 
-n_val :: Num->Z
+n_val :: Num -> Z
 n_val n = n
 
-a_val :: Aexp->State->Z
+a_val :: Aexp -> State -> Z
 a_val (Num n) s = n_val n
 a_val (Var x) s = s x
 a_val (Mult aexp1 aexp2) s = (a_val aexp1 s) * (a_val aexp2 s)
 a_val (Add  aexp1 aexp2) s = (a_val aexp1 s) + (a_val aexp2 s)
 a_val (Sub  aexp1 aexp2) s = (a_val aexp1 s) - (a_val aexp2 s)
 
-b_val :: Bexp->State->T
+b_val :: Bexp -> State-> T
 b_val TRUE  s = True
 b_val FALSE s = False
 b_val (Neg  bexp) s        = not (b_val bexp s)
@@ -27,28 +26,26 @@ b_val (And  bexp1 bexp2) s = ( b_val bexp1 s ) && ( b_val bexp2 s )
 b_val (Eq   aexp1 aexp2) s = ( a_val aexp1 s ) == ( a_val aexp2 s )
 b_val (Less aexp1 aexp2) s = ( a_val aexp1 s ) <= ( a_val aexp2 s )
 
-update :: State->Z->Var->State
+update :: State -> Z-> Var-> State
 update s i v x
     | (x == v)  = i
     | otherwise = s x
 
-cond :: (a->T, a->a, a->a) -> (a->a)
+cond :: (a -> T, a -> a, a -> a) -> (a -> a)
 cond (b, c, d) x
     | b x == True = c x
     | otherwise   = d x
 
-fix :: ( (State->State)->(State->State) )->(State->State)
+fix :: ((State -> State) -> (State->State)) -> (State -> State)
 fix ff = ff (fix ff)
 
-s_ds :: Stm->State->State
+s_ds :: Stm -> State -> State
 s_ds Skip             s = s
 s_ds (Ass x a)        s = update s (a_val a s) x
 s_ds (Comp stm1 stm2) s = ((s_ds stm2) . (s_ds stm1)) s
 s_ds (If b stm1 stm2) s = cond(b_val b, s_ds stm1, s_ds stm2) s
 s_ds (While b stm)    s = (fix f) s where
     f g = cond(b_val b, g . (s_ds stm), id)
-
--------------------------------------------------------------------------------
 
 -- Initialize everything to 0
 initialState :: State
