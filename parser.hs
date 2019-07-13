878a9fa4d4f135d9f7f91a3ae5392f6139730787
Andrei Nitu <an16228@my.bristol.ac.uk>
2019-07-13 16:47:47 +0100

    Complete parser

--- parser.hs
+++ parser.hs
@@ -1,6 +1,77 @@
-import Yoda
+module Parser where
 
---newtype Parser a = Parser { parse :: String -> [(String, a)] }
+import Yoda
+import Data.List
+import Prelude hiding (Num)
+
+-- syntactic categories Num and Var
+type Num = Integer
+type Var = String
+
+-- semantic types Z and T
+type Z = Integer
+type T = Bool
+
+data Aexp = Num  Num
+          | Var  Var
+          | Mult Aexp Aexp
+          | Add  Aexp Aexp
+          | Sub  Aexp Aexp
+ deriving (Show, Eq, Read)
+
+data Bexp = TRUE
+          | FALSE
+          | Neg  Bexp
+          | And  Bexp Bexp
+          | Eq   Aexp Aexp
+          | Less Aexp Aexp
+ deriving (Show, Eq, Read)
+
+data Stm = Skip
+         | Ass Var Aexp
+         | Comp Stm Stm
+         | If Bexp Stm Stm
+         | While Bexp Stm
+  deriving (Show, Eq, Read)
+
+-- The while language is a parser of |Stm| terms. We allow multiple
+-- semi-colons at the top level.
+while :: Parser Stm
+while = stms
+
+precedence :: [Parser (a -> a -> a)] -> Parser a -> Parser a
+precedence ops arg = foldl' build arg ops
+  where build term ops = chainl term ops
+
+aexp = precedence [Mult <$ tok "*"
+                  ,Add  <$ tok "+" <|> Sub <$ tok "-" ]
+     ( Num <$> num
+   <|> Var <$> var
+   <|> tok "(" *> aexp <* tok ")")
+
+bexp :: Parser Bexp
+bexp = precedence [And <$ tok "&"]
+      ( TRUE  <$  tok "true"
+    <|> FALSE <$  tok "false"
+    <|> Eq    <$> aexp <* tok "=" <*> aexp
+    <|> Less  <$> aexp <* tok "<=" <*> aexp
+    <|> Neg   <$  tok "!" <*> bexp
+    <|> tok "(" *> bexp <* tok ")")
+
+stms :: Parser Stm
+stms = chainl stm (Comp <$ tok ";")
+
+stm = Ass   <$> var <* tok ":=" <*> aexp
+  <|> Skip  <$  tok "skip"
+  <|> If    <$  tok "if" <*> bexp <* tok "then" <*> stm <* tok "else" <*> stm
+  <|> While <$  tok "while" <*> bexp <* tok "do" <*> stm
+  <|> tok "(" *> stms <* tok ")"
+
+chainl p op = p >>= rest where
+  rest x = do f <- op
+              y <- p
+              rest (f x y)
+       <|> return x
 
 whitespace :: Parser ()
 whitespace = many (oneOf [' ', '\t']) *> pure ()
@@ -8,5 +79,8 @@ whitespace = many (oneOf [' ', '\t']) *> pure ()
 tok :: String -> Parser String
 tok xs = whitespace *> string xs <* whitespace
 
-number :: Parser Int
-number = undefined
+num :: Parser Integer
+num = read <$> some (oneOf ['0' .. '9']) <* whitespace
+
+var :: Parser String
+var = some (oneOf ['a' .. 'z']) <* whitespace
--- while.hs
+++ while.hs
@@ -1,89 +1,36 @@
-
 import Prelude hiding (Num)
 import qualified Prelude (Num)
-
--- syntactic categories Num and Var
-type Num = Integer
-type Var = String
-
--- semantic types Z and T
-type Z = Integer
-type T = Bool
+import Data.List
+import Data.Maybe
+import Parser
+import Yoda
 
 -- Set of all possible states as the set of
 -- all possible functions from variables to integers
 type State = Var->Z
 
-data Aexp = N Num | V Var
-          | Mult Aexp Aexp
-          | Add  Aexp Aexp
-          | Sub  Aexp Aexp
- deriving (Show, Eq, Read)
-
-data Bexp = TRUE | FALSE
-          | Neg Bexp
-          | And Bexp Bexp
-          | Eq  Aexp Aexp
-          | Le  Aexp Aexp
- deriving (Show, Eq, Read)
-
 n_val :: Num->Z
 n_val n = n
 
-s :: State
-s "x" = 1
-s "y" = 2
-s "z" = 3
-s  v  = 0
-
-a :: Aexp
-a = Mult ( Add (V "x") (V "y") ) ( Sub (V "z") (N 1) )
-
 a_val :: Aexp->State->Z
-a_val (N n) s = n_val n
-a_val (V x) s = s x
+a_val (Num n) s = n_val n
+a_val (Var x) s = s x
 a_val (Mult aexp1 aexp2) s = (a_val aexp1 s) * (a_val aexp2 s)
 a_val (Add  aexp1 aexp2) s = (a_val aexp1 s) + (a_val aexp2 s)
 a_val (Sub  aexp1 aexp2) s = (a_val aexp1 s) - (a_val aexp2 s)
 
-b :: Bexp
-b = Neg ( Eq (Add (V "x") (V "y") ) (N 4) )
-
 b_val :: Bexp->State->T
 b_val TRUE  s = True
 b_val FALSE s = False
-b_val (Neg bexp) s        = not (b_val bexp s)
-b_val (And bexp1 bexp2) s = ( b_val bexp1 s ) && ( b_val bexp2 s )
-b_val (Eq  aexp1 aexp2) s = ( a_val aexp1 s ) == ( a_val aexp2 s )
-b_val (Le  aexp1 aexp2) s = ( a_val aexp1 s ) <= ( a_val aexp2 s )
-
-data Stm = Skip
-         | Ass Var Aexp
-         | Comp Stm Stm
-         | If Bexp Stm Stm
-         | While Bexp Stm
- deriving (Show, Eq, Read)
-
--- y:=1;
--- while ¬(x=1) do
--- y:=y*x;
--- x:=x-1;
-
-p :: Stm
-p = (Comp
- (Ass "y" (N 1))
- (While (Neg (Eq (V "x") (N 1)))
-   (Comp
-   (Ass "y" (Mult (V "y") (V "x")))
-   (Ass "x" (Sub  (V "x") (N 1) )))))
+b_val (Neg  bexp) s        = not (b_val bexp s)
+b_val (And  bexp1 bexp2) s = ( b_val bexp1 s ) && ( b_val bexp2 s )
+b_val (Eq   aexp1 aexp2) s = ( a_val aexp1 s ) == ( a_val aexp2 s )
+b_val (Less aexp1 aexp2) s = ( a_val aexp1 s ) <= ( a_val aexp2 s )
 
 update :: State->Z->Var->State
 update s i v x
     | (x == v)  = i
     | otherwise = s x
-
-s' :: State
-s' = update s 5 "x"
 
 cond :: (a->T, a->a, a->a) -> (a->a)
 cond (b, c, d) x
@@ -100,3 +47,21 @@ s_ds (Comp stm1 stm2) s = ((s_ds stm2) . (s_ds stm1)) s
 s_ds (If b stm1 stm2) s = cond(b_val b, s_ds stm1, s_ds stm2) s
 s_ds (While b stm)    s = (fix f) s where
     f g = cond(b_val b, g . (s_ds stm), id)
+
+-------------------------------------------------------------------------------
+
+-- Initialize everything to 0
+initialState :: State
+initialState _ = 0
+
+-- Allow variables a - z
+vars :: [Var]
+vars = map (\x -> x:[]) ['a'..'z']
+
+-- Parse and evaluate program
+eval :: String -> [Z]
+eval pr = map (s_ds (snd (parse stms pr !! 0)) initialState) vars
+
+-- Get value of a variable
+val :: Char -> [Z] -> Z
+val var result = result !! (fromMaybe 0 (findIndex (==var) ['a' .. 'z']))
